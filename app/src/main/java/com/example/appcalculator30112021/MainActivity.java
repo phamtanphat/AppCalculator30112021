7e19310117c68dc7aabd03b3254040dad2324388
Hoaipt1998 <54972641+Hoaipt1998@users.noreply.github.com>
2021-12-21 20:44:29 +0700

    Thanh Hoai nop bai

--- app/src/main/java/com/example/appcalculator30112021/MainActivity.java
+++ app/src/main/java/com/example/appcalculator30112021/MainActivity.java
@@ -4,29 +4,174 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText mEdtNumber1,mEdtNumber2;
-    TextView mTvResult;
-    Button mBtnPlus,mBtnMinus,mBtnDivision,mBtnMultiplication;
-
+    EditText mnumber1 , mnumber2;
+    TextView mresult;
+    Button mbuttonAdd, mbuttonMinus , mbuttonMultipli, mbuttonDev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ánh xạ
-        mEdtNumber1 = findViewById(R.id.edittextNumber1);
-        mEdtNumber2 = findViewById(R.id.edittextNumber2);
-        mTvResult = findViewById(R.id.textViewResult);
-        mBtnPlus = findViewById(R.id.buttonPlus);
-        mBtnMinus = findViewById(R.id.buttonMinus);
-        mBtnDivision = findViewById(R.id.buttonDivision);
-        mBtnMultiplication = findViewById(R.id.buttonMultiplication);
+        //anh xa
+        mnumber1 = findViewById(R.id.number1);
+        mnumber2 = findViewById(R.id.number2);
+        mresult = findViewById(R.id.result);
+        mbuttonAdd = findViewById(R.id.buttonAdd);
+        mbuttonMinus = findViewById(R.id.buttonMinus);
+        mbuttonMultipli = findViewById(R.id.buttonMultipli);
+        mbuttonDev = findViewById(R.id.buttonDev);
+
+        //Hàm kiểm tra giá trị
+//        int a = 5;
+//
+//        Log.d("BBB",String.valueOf(a) );
+        //Hàm hiển thị thông báo
+        Toast.makeText(MainActivity.this, "Phạm Hoàng Thư", Toast.LENGTH_SHORT).show();
+        //bắt sự kiện
+        mbuttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //lấy dữ liệu input
+                String number1 = mnumber1.getText().toString();
+                String number2 = mnumber2.getText().toString();
+                if(number1.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ nhất đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else if(number2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ hai đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String result = String.valueOf((Float.valueOf(number1)  + Float.valueOf(number2))) ;
+                    mresult.setText(result);
+
+                }
+
+                Log.d("BBB", "text number 1: " + number1);
+                Log.d("BBB", "text number 2: " + number2);
+
+
+            }
+        });
+        mbuttonDev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //lấy dữ liệu input
+                String number1 = mnumber1.getText().toString();
+                String number2 = mnumber2.getText().toString();
+                String result;
+                if(number1.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ nhất đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else if(number2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ hai đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else if(number2.equals(0))
+                {
+                    Toast.makeText(MainActivity.this, "Không chia cho 0 được nhoa", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    result = String.valueOf((Float.valueOf(number1) / Float.valueOf(number2)));
+                    mresult.setText(result);
+
+                }
+
+                Log.d("BBB", "text number 1: " + number1);
+                Log.d("BBB", "text number 2: " + number2);
+
+
+            }
+
+        });
+        mbuttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //lấy dữ liệu input
+                String number1 = mnumber1.getText().toString();
+                String number2 = mnumber2.getText().toString();
+                String result;
+                if(number1.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ nhất đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else if(number2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ hai đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    result = String.valueOf((Float.valueOf(number1) - Float.valueOf(number2)));
+                    mresult.setText(result);
+
+                }
+
+                Log.d("BBB", "text number 1: " + number1);
+                Log.d("BBB", "text number 2: " + number2);
+
+
+            }
+
+        });
+        mbuttonMultipli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //lấy dữ liệu input
+                String number1 = mnumber1.getText().toString();
+                String number2 = mnumber2.getText().toString();
+                String result;
+                if(number1.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ nhất đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else if(number2.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Nhập vô ô thứ hai đi kìa", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                    result = String.valueOf((Float.valueOf(number1) * Float.valueOf(number2)));
+                    mresult.setText(result);
+
+                }
+
+                Log.d("BBB", "text number 1: " + number1);
+                Log.d("BBB", "text number 2: " + number2);
+
+
+            }
+
+        });
+
+
+
+
+//        //tham tri
+//        int a = 5;
+//        int b = 10;
+//
+//        a= b;
+//        //b thay dooir a ko thay ddoi
+
+        //thham chieu
+
+//        Object obj1 = new Object();
+//        Log.d("BBB" , "Before obj 1" +obj1);
+//        Object obj2 = new Object();
+//        Log.d("BBB" , "Before obj 2" + obj2);
+//        obj1 =obj2;
+//        Log.d("BBB" , "after obj 1" + obj1);
+//        Log.d("BBB" , "after obj 2"+ obj2);
 
     }
 }
