3a2c26e9259eaa6795b4c9d399c31d0f430a264f
phamtanphat <phatdroid94@gmail.com>
2021-12-23 18:10:39 +0700

    plus

--- app/src/main/java/com/example/appcalculator30112021/MainActivity.java
+++ app/src/main/java/com/example/appcalculator30112021/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity{
 
     EditText mEdtNumber1,mEdtNumber2;
     TextView mTvResult;
@@ -28,5 +32,62 @@ public class MainActivity extends AppCompatActivity {
         mBtnDivision = findViewById(R.id.buttonDivision);
         mBtnMultiplication = findViewById(R.id.buttonMultiplication);
 
+
+        // Hàm kiểm tra giá trị
+//        int a = 5;
+//        Log.d("BBB",a + "");
+        // Hàm in thông báo
+
+
+//        Toast.makeText(MainActivity.this,"Xin chào",Toast.LENGTH_SHORT).show();
+
+        // bắt sự kiện
+        mBtnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy dữ liêu input
+                String textNumber1 = mEdtNumber1.getText().toString();
+                String textNumber2 = mEdtNumber2.getText().toString();
+
+                if (textNumber1.isEmpty() || textNumber2.isEmpty()){
+                    if(textNumber1.isEmpty()){
+                        mEdtNumber1.setError("Input empty");
+                    }
+                    if (textNumber2.isEmpty()){
+                        mEdtNumber2.setError("Input empty");
+                    }
+                    return;
+                }
+
+                int number1 = Integer.parseInt(textNumber1);
+                int number2 = Integer.parseInt(textNumber2);
+                int result = number1 + number2;
+                mTvResult.setText("Result = " + result);
+            }
+        });
+        mBtnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy dữ liêu input
+                String textNumber1 = mEdtNumber1.getText().toString();
+                String textNumber2 = mEdtNumber2.getText().toString();
+
+                if (textNumber1.isEmpty() || textNumber2.isEmpty()){
+                    if(textNumber1.isEmpty()){
+                        mEdtNumber1.setError("Input empty");
+                    }
+                    if (textNumber2.isEmpty()){
+                        mEdtNumber2.setError("Input empty");
+                    }
+                    return;
+                }
+
+                int number1 = Integer.parseInt(textNumber1);
+                int number2 = Integer.parseInt(textNumber2);
+                int result = number1 + number2;
+                mTvResult.setText("Result = " + result);
+            }
+        });
     }
+
 }
